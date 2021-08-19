edbd1ac4d9ee84ff0434c17df00cc7efa875a685
Tamara Erby <tlynnerby@yahoo.com>
2021-08-19 19:46:52 -0400

    new commit

--- DisneyFilmStore.Data/Order.cs
+++ DisneyFilmStore.Data/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace DisneyFilmStore.Data
 {
     public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double TotalOrderCost { get; set; }
+
+        [Required, ForeignKey(nameof(CustomerId))]
+        public int CustomerId { get; set; }
     }
 }
