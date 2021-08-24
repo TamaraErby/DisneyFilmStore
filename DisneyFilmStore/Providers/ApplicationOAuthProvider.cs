279a97e0d49f291a808eb1af5d9b3db3dc9df008
Dean Collier <daccollier@gmail.com>
2021-08-24 19:14:17 -0400

    all errors are fixed

--- DisneyFilmStore/Providers/ApplicationOAuthProvider.cs
+++ DisneyFilmStore/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using DisneyFilmStore.Models;
+using DisneyFilmStore.Data;
 
 namespace DisneyFilmStore.Providers
 {
