6cc3dba2ad0d1085a411748bcb808abc3de056db
Mariah Page <mariahlynnpage@gmail.com>
2021-08-24 20:56:47 -0400

    new commit

---
+++ DisneyFilmStore.Models/FilmModels/FilmCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisneyFilmStore.Models.FilmModels
+{
+    public class FilmCreate
+    {
+        [Required]
+        public int FilmId { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+    }
+}
---
+++ DisneyFilmStore.Models/FilmModels/FilmEdit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisneyFilmStore.Models.FilmModels
+{
+    public class FilmEdit
+    {
+        [Required]
+        public int FilmId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public DateTime YearReleased { get; set; }
+        [Required]
+        public double MemberCost { get; set; }
+        [Required]
+        public double NonMemberCost { get; set; }
+    }
+}
